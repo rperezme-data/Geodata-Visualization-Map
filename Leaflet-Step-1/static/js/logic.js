f0ccdb1930968623038e3f0cf9adec6dec682bdb
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-07-13 12:38:26 -0500

    Add getColor Function & Add Legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -5,6 +5,17 @@
 //     [83.162102, -52.233040]  //Northeast
 // ];
 
+// CHOOSE COLOR FUNCTION (*Reference: https://leafletjs.com/examples/choropleth/)
+function getColor(d) {
+    return d > 90  ? '#FF1100' :
+           d > 70  ? '#FF4B00' :
+           d > 50  ? '#FF9500' :
+           d > 30  ? '#FFC000' :
+           d > 10  ? '#FFFF00' :
+           d > -10 ? '#00F000' :
+                     'blue';
+}
+
 
 // CREATE MAP FUNCTION
 function createMap(earthquakes) {
@@ -12,16 +23,16 @@ function createMap(earthquakes) {
     // console.log("Creating map");   // DEBUG
 
     // Tile layer (background map)
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "light-v10",
         accessToken: API_KEY
     });
-    
+
     // Tile layers holder (baseMaps object)
     var baseMaps = {
-        "Light Map": lightmap
+        "Light": lightMap
     };
 
     // Additional layers holder (overlayMaps object)
@@ -31,9 +42,9 @@ function createMap(earthquakes) {
 
     // Build map & Set parameters (map object)
     var myMap = L.map("map", {
-        center: [25, -25],
-        zoom: 2.5,
-        layers: [lightmap, earthquakes]
+        center: [25, -15],
+        zoom: 3,
+        layers: [lightMap, earthquakes]
     });
 
     // Layer control
@@ -42,6 +53,29 @@ function createMap(earthquakes) {
     }).addTo(myMap); // Add to map
 
     // console.log("Done!");   // DEBUG
+
+
+// CREATE LEGEND FUNCTION (*Reference: https://leafletjs.com/examples/choropleth/)
+var legend = L.control({ position: 'bottomright' });
+    
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+    console.log("Legend: ", legend);   // DEBUG
+    legend.addTo(myMap);
 
 }
 
@@ -76,17 +110,17 @@ function createMarkers(response) {
         // Build earthquake marker
         var earthquakeMarker = L.circleMarker(location, {
             radius: magnitude * 3,
-        // var earthquakeMarker = L.circle(location, {
+            // var earthquakeMarker = L.circle(location, {
             // radius: magnitude * 2* 10000,
             stroke: true,
             color: "black",
             weight: 1,
-            fillColor: "yellow",
-            fillOpacity: 0.5
+            fillColor: getColor(depth),
+            fillOpacity: 0.75
         }).bindPopup(`<strong>Magnitude: ${magnitude}</strong><hr>Location: ${place}<br>Depth: ${depth} km`);
 
         earthquakeMarkers.push(earthquakeMarker);
-        
+
         // console.log("Marker: ", earthquakeMarker);   // DEBUG
         magnitudeArr.push(magnitude);   // DEBUG
         depthArr.push(depth);   // DEBUG
